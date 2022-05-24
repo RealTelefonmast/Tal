36d747d4d12ab20abd94390c1eec858952431d20
Maxim <MaximSw@web.de>
2022-05-24 11:41:31 +0200

    BiomeTest

---
+++ Source/VilousTal/BiomeWorker_Talyxian.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace VilousTal
+{
+    public class BiomeWorker_Talyxian : BiomeWorker
+    {
+        public override float GetScore(Tile tile, int tileID)
+        {
+            return 1;
+        }
+    }
+}
